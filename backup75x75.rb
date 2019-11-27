6166ae1bf459f59a1823ffed7138378af745d15c
Roland Tanglao <roland@rolandtanglao.com>
2019-11-26 22:14:01 -0800

    add scrpts to get 75px by 75 px

---
+++ backup75x75.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'json'
+require 'curb'
+require 'pp'
+require 'time'
+require 'date'
+require 'uri'
+
+
+def fetch_1_at_a_time(urls_filenames)
+
+  easy = Curl::Easy.new
+  easy.follow_location = true
+
+  urls_filenames.each do|url_fn|
+    easy.url = url_fn["url"]
+    filename = url_fn["filename"]
+    $stderr.print "filename:'#{filename}'"
+    $stderr.print "url:'#{url_fn["url"]}'"
+    if File.exist?(filename)
+      $stderr.printf("skipping EXISTING filename:%s\n", filename)
+      next
+    end
+    try_count = 0
+    begin
+      File.open(filename, 'wb') do|f|
+        easy.on_progress {|dl_total, dl_now, ul_total, ul_now| $stderr.print "="; true }
+        easy.on_body {|data| f << data; data.size }
+        easy.perform
+        $stderr.puts "=> '#{filename}'"
+      end
+    rescue Curl::Err::ConnectionFailedError => e
+      try_count += 1
+      if try_count < 4
+        $stderr.printf("Curl:ConnectionFailedError exception, retry:%d\n",\
+                       try_count)
+        sleep(10)
+        retry
+      else
+        $stderr.printf("Curl:ConnectionFailedError exception, retrying FAILED\n")
+        raise e
+      end
+    end
+  end
+end
+
+urls_filenames = []
+
+i = 1
+ARGF.each do |url|
+  url = url.chomp 
+  filename = sprintf("%4.4d-rt-berlin-july2019-75px-75px.jpg", i)
+  next if url.nil?
+  i += 1
+  urls_filenames.push({"url"=> url, "filename" => filename}) 
+end
+
+$stderr.printf("FETCHING:%d originals\n", urls_filenames.length)
+
+fetch_1_at_a_time(urls_filenames)
+
